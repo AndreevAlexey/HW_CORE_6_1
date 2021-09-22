c52607b31dd10c7ee4fccc5644f3a950eaa6ef79
Andreev Alexey <andreev-j@yandex.ru>
2021-09-22 17:57:58 +0700

    Hamcrest added

--- src/test/java/com/company/ContactsTest.java
+++ src/test/java/com/company/ContactsTest.java
@@ -1,36 +1,45 @@
 package com.company;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class ContactsTest {
-    private Contact contact1;
-    private Contact contact2;
-    private Contact contact3;
-    private Contacts contacts;
+    Contact contact1;
+    Contact contact2;
+    Contact contact3;
+    Contacts contacts;
 
-    @BeforeAll
-    public void setUp() throws Exception {
+    @BeforeEach
+    public void init()  {
         contact1 = new Contact("A1","AA1","11111",Group.WORK);
         contact2 = new Contact("B2","BB2","22222",Group.WORK);
         contact3 = new Contact("C3","CC3","33333",Group.WORK);
-        Contacts contacts = new Contacts();
-    }
-
-    @Test
-    void addContact() {
-
+        contacts = new Contacts();
     }
 
     @Test
     void replaceContact() {
+        // given
+        contacts.addContact(contact1);
+        contacts.addContact(contact2);
+        contacts.addContact(contact3);
+        Contact actual = new Contact("D4","DD4","33333",Group.HOME);
+        contacts.replaceContact(actual);
+        // when
+        Contact result = contacts.getContactByNumber("33333");
+        // then
+        assertThat(result, notNullValue());
+        assertThat(result, hasProperty("lastName", equalTo("DD4")));
+        assertThat(result, equalTo(actual));
     }
 
     @Test
-    void deleteContact() {
+    public void deleteContact() {
         // given
         contacts.addContact(contact1);
         contacts.addContact(contact2);
@@ -42,6 +51,9 @@ class ContactsTest {
         // then
         Assertions.assertNotNull(actual);
         Assertions.assertNull(result);
+        // Hamcrest
+        assertThat(actual, notNullValue());
+        assertThat(result, nullValue());
     }
 
     @Test
@@ -62,6 +74,8 @@ class ContactsTest {
         Contact result = contacts.getContactByNumber("22222");
         // then
         Assertions.assertNull(result);
+        // Hamcrest
+        assertThat(result, nullValue());
     }
     @Test
     void getContactByNumber_NULL_CONTACT() {
@@ -70,8 +84,22 @@ class ContactsTest {
         Contact result = contacts.getContactByNumber(null);
         // then
         Assertions.assertNull(result);
+        // Hamcrest
+        assertThat(result, nullValue());
     }
     @Test
     void existInContacts() {
+        // given
+        contacts.addContact(contact1);
+        contacts.addContact(contact2);
+        contacts.addContact(contact3);
+        // when
+        boolean result = contacts.existInContacts("22222");
+        // then
+        assertThat(result, Matchers.equalTo(true));
+        // when
+        result = contacts.existInContacts("55555");
+        // then
+        assertThat(result, Matchers.equalTo(false));
     }
 }
